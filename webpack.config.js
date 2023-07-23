0db49d7e6ba17398311e141837ca1da0e397539d
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-23 23:27:00 +0100

    project inititalization

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: '/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '/src/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
