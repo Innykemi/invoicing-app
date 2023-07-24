beff87591af0371c83cd0a64b12ca28f631fac85
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-24 03:43:47 +0100

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,6 @@ const path = require('path');
 
 module.exports = {
   entry: '/src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
   module: {
     rules: [
       {
