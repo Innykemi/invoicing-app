c636877f7dc60895d8ad1c0bb2ebb270fd803395
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-24 03:11:50 +0100

    update files

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main.jsx';
+import { setup } from "goober";
+import { GlobalStyles } from '../globalStyle.jsx';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+setup(React.createElement);
+
+ReactDOM.render(
+  <React.Fragment>
+    <Main />
+    <GlobalStyles />
+  </React.Fragment>,
+  document.getElementById('root')
+);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Invoice from './pages/Invoice.jsx';
 
 function Main() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invoice" element={<Invoice />} />
+      </Routes>
+    </Router>
   );
 }
 
