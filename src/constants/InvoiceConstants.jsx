718c6a90aeb8ac2b90c847d82f4334004bf09c76
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-26 16:28:51 +0100

    add invoice constants

---
+++ src/constants/InvoiceConstants.jsx
@@ -0,0 +1,9 @@
+const InvoiceConstants = {
+  CREATE_INVOICE: "CREATE_INVOICE",
+  ADD_LINE_ITEM: "ADD_LINE_ITEM",
+  SEND_INVOICE: "SEND_INVOICE",
+  UPDATE_INVOICE: "UPDATE_INVOICE",
+  DELETE_INVOICE: "DELETE_INVOICE",
+};
+
+export default InvoiceConstants;
