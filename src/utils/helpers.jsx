df3aedc5bc2abd6f212f28de7db7f2105225d5fe
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-26 18:14:08 +0100

    update

--- src/utils/helpers.jsx
+++ src/utils/helpers.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
 export const createWrapperAndAppendToBody = (wrapperId) => {
   const wrapperElement = document.createElement("div");
   wrapperElement.setAttribute("id", wrapperId);
   document.body.appendChild(wrapperElement);
   return wrapperElement;
+};
+
+export const useWindowResize = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowWidth;
 };
