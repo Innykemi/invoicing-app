562dd7aa2c888956cd4b61295108cc61118dffbb
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-26 04:29:56 +0100

    add icon

---
+++ src/assets/images/DeleteIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DeleteIcon = () => (
+  <svg width="13" height="16" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+    <path d="M837.312 227.584v682.624c0 62.848-50.88 113.792-113.728 113.792h-455.168c-62.81 0-113.728-50.918-113.728-113.728 0-0.023 0-0.045 0-0.068l-0 0.004v-682.624h682.624zM638.272 0l56.832 56.896h199.104v113.792h-796.416v-113.792h199.040l57.024-56.896h284.416z" fill="currentcolor" fillRule="nonzero" />
+  </svg>
+);
+
+export default DeleteIcon;
