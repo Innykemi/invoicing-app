7fe3f79f0695072aeea3fc9e76627c3d65430fbb
Oluwakemi Raimi <oluwakemiraimi.nig@gmail.com>
2023-07-26 18:09:47 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: './bundle.js',
+    publicPath: '/', 
+  },
   module: {
     rules: [
       {
